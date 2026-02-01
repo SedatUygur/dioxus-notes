a5253c62dc8b02fc1fdf093065654e9e46c0e96e
Sedat Uygur <sedat.uygur@outlook.com>
2026-02-01 23:37:50 +0300

    feat(backend): add app state and db memo

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use sqlx::{PgPool, FromRow};
+use uuid::Uuid;
+use chrono::{Utc, DateTime};
+/* use axum::{
+    extract::{Path, State},
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, post, delete},
+    Json, Router,
+};
+use serde_json::json;
+use std::net::SocketAddr;
+use dotenvy::dotenv;
+use std::env;
+use tower_http::cors::{Any, CorsLayer};
+use tracing_subscriber::{fmt, EnvFilter};
+use core::{Memo, NewMemo}; */
+
+#[derive(Clone)]
+struct AppState {
+    db: PgPool,
+}
+
+#[derive(Debug, FromRow)]
+struct DbMemo {
+    id: Uuid,
+    title: Option<String>,
+    content: String,
+    public: bool,
+    created_at: DateTime<Utc>,
+    updated_at: DateTime<Utc>,
 }
